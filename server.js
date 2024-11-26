63110e5284ce41fd8860bc618bcc19b529f6a800
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2024-11-26 09:41:55 +0300

    feat/user

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const fileRoutes = require("./routes/fileRoutes");
+const  router = require('./routes/User')
 
 const app = express();
 app.use(express.json());
 
 connectDB();
 app.use("/files", fileRoutes);
+app.use('/', router)
 
 const PORT = 3000;
 app.listen(PORT, () => {
